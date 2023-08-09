9aa9c6df8384bb62187f0bfe21c2e314220332d9
Nicholas Engleman <nicholas.engleman@ajc.com>
2023-08-09 17:14:47 -0400

    added back error page

---
+++ pages/_error.page.tsx
@@ -0,0 +1,9 @@
+export const runtime = 'experimental-edge'
+
+import Page from '../components/global/Page/Page'
+
+const ErrorPage = () => {
+  return <Page>There was an Error!</Page>
+}
+
+export default ErrorPage
