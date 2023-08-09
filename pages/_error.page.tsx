02a136e995b68b4552eb5a6038289db82c4d316d
Nicholas Engleman <nicholas.engleman@ajc.com>
2023-08-09 17:34:10 -0400

    removed error page

--- pages/_error.page.tsx
+++
@@ -1,9 +0,0 @@
-export const runtime = 'experimental-edge'
-
-import Page from '../components/global/Page/Page'
-
-const ErrorPage = () => {
-  return <Page>There was an Error!</Page>
-}
-
-export default ErrorPage
