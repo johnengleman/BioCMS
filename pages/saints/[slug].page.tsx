1e95b016fe1f410a4c2c3f48bf874c2535a7cc22
Nicholas Engleman <nicholas.engleman@ajc.com>
2023-08-12 21:05:01 -0400

    disabled related saints

--- pages/saints/[slug].page.tsx
+++ pages/saints/[slug].page.tsx
@@ -85,30 +85,27 @@ export const getStaticProps = async ({ params }) => {
     getSaint(slug),
   )
 
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/getRelatedSaints/?slug=${slug}`,
-  )
+  // const res = await fetch(
+  //   `${process.env.NEXT_PUBLIC_API_URL}/api/getRelatedSaints/?slug=${slug}`,
+  // )
 
-  if (!res.ok) {
-    throw new Error(`HTTP error! Status: ${res.status}`)
-  }
+  // if (!res.ok) {
+  //   throw new Error(`HTTP error! Status: ${res.status}`)
+  // }
 
-  let relatedSaints
+  // let relatedSaints
 
-  try {
-    relatedSaints = await res.json()
-  } catch (error) {
-    console.error('Error parsing JSON:', res.text())
-    relatedSaints = null
-  }
+  // try {
+  //   relatedSaints = await res.json()
+  // } catch (error) {
+  //   console.error('Error parsing JSON:', res.text())
+  //   relatedSaints = null
+  // }
 
   return {
     props: {
       dehydratedState: dehydrate(queryClient),
-      relatedSaints: relatedSaints
-        .filter((saint) => saint.slug !== slug)
-        .sort(() => Math.random() - 0.5)
-        .slice(0, 4),
+      relatedSaints: [],
     },
     revalidate: 60,
   }
