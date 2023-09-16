b4834b7bdf50409cf7af48c402231bbcb7e76e82
Nicholas Engleman <nicholas.engleman@ajc.com>
2023-09-16 10:10:13 -0400

    added socal links

--- components/page/SMButtons/SMButtons.tsx
+++ components/page/SMButtons/SMButtons.tsx
@@ -1,32 +1,36 @@
+import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faSquareXTwitter,
   faSquareFacebook,
 } from '@fortawesome/free-brands-svg-icons'
-import { faQuestion } from '@fortawesome/pro-duotone-svg-icons'
 
 import * as S from './styles'
 
 const SMButtons = ({ transparent }) => (
   <S.SocialMediaButtons>
-    <div className="button">
-      <FontAwesomeIcon
-        icon={faSquareXTwitter}
-        style={{
-          color: transparent ? '#ffffff' : '#ccad00',
-          fontSize: '35px',
-        }}
-      />
-    </div>
-    <div className="button">
-      <FontAwesomeIcon
-        icon={faSquareFacebook}
-        style={{
-          color: transparent ? '#ffffff' : '#ccad00',
-          fontSize: '35px',
-        }}
-      />
-    </div>
+    <Link href="https://twitter.com/findasaint">
+      <div className="button">
+        <FontAwesomeIcon
+          icon={faSquareXTwitter}
+          style={{
+            color: transparent ? '#ffffff' : '#ccad00',
+            fontSize: '35px',
+          }}
+        />
+      </div>
+    </Link>
+    <Link href="https://www.facebook.com/groups/findasaint">
+      <div className="button">
+        <FontAwesomeIcon
+          icon={faSquareFacebook}
+          style={{
+            color: transparent ? '#ffffff' : '#ccad00',
+            fontSize: '35px',
+          }}
+        />
+      </div>
+    </Link>
   </S.SocialMediaButtons>
 )
 
